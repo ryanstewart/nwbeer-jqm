d77e1523aa3161074d99502295229b03b10925c0
Ryan Stewart <ryan@ryanstewart.net>
2012-05-31 12:05:00 -0700

    Style changes and JS changes

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
-$(function() {
-  document.addEventListener("deviceready", onDeviceReady, false);
-});
-
-function onDeviceReady() {
-    alert('cordova is ready');
-}
+window.addEventListener('load', function () {
+    document.addEventListener('deviceready', function () {
+        alert("PhoneGap is now loaded!");
+    }, false);
+}, false);
 
 
 $(document).bind("mobileinit", function(){
     if((navigator.userAgent.match(/iPhone/i)) ||(navigator.userAgent.match(/iPod/i))) {
+        // use the softkey back button
         $.mobile.page.prototype.options.addBackBtn= true;
-        $.mobile.defaultPageTransition = "slide";        
+
+        // make slide the default trasition
+        $.mobile.defaultPageTransition = "slide";      
+        
+        // use support for better scrolling on iOS
+        $.mobile.touchOverflowEnabled = true;
     } else if (navigator.userAgent.match(/Android/i)){
+        // make Android transition the pop effect
         $.mobile.defaultPageTransition = "pop";      
     }   
 });
