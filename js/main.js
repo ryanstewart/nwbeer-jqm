b3b74d3f0c21fe649f3932b3c42edd91564624cf
Ryan Stewart <ryan@ryanstewart.net>
2012-05-31 09:03:25 -0700

    Working with deviceready event

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
-$(document).bind("mobileinit", function(){
-    $(document).bind("deviceready", function (){
-        var platform = device.platform;
-        navigator.notification.alert(platform);
-        switch(platform) {
-            case "Android":
-                $.mobile.defaultPageTransition = "pop";
-                break;
-            case "iOS":
-                $.mobile.page.prototype.options.addBackBtn= true;
-                $.mobile.defaultPageTransition = "slide";
-                break;
-        }
-    });
+var platform;
 
+function init() {
+    document.addEventListener("deviceready",onDeviceReady);
+}
+
+onDeviceReady(e) {
+    platform = device.platform;
+    navigator.notification.alert(platform);
+}
+
+$(document).bind("mobileinit", function(){
+    alert('test');
+    switch(platform) {
+        case "Android":
+            $.mobile.defaultPageTransition = "pop";
+            break;
+        case "iOS":
+            $.mobile.page.prototype.options.addBackBtn= true;
+            $.mobile.defaultPageTransition = "slide";
+            break;
+    }
 });
 
 
